ac452c402dd13ab7e72222e6a1e57f44350817ab
chinhnhan123 <nhanchinh113@gmail.com>
2023-02-08 20:49:03 +0700

    feat: create and edit idea

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("homeStaff", { title: "Express" });
+  res.render("editIdea", { title: "Express" });
 });
 
 module.exports = router;
