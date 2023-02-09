123972ec0d4d70186130ace8ce81c9c1b7cb1e76
LePhuongTrung <trunglp2001@gmail.com>
2023-02-09 19:09:56 +0700

    feat: CRUD repository

---
+++ repositories/idea.repository.js
@@ -0,0 +1,48 @@
+const ideaModel = require("../database/models/Idea");
+
+const createIdea = async (data) => {
+  try {
+    const Idea = await ideaModel.create(data);
+    return Idea;
+  } catch (err) {
+    console.error("🚀 ~ file: idea.repository.js:8 ~ createIdea ~ err", err);
+    return err;
+  }
+};
+
+const readIdea = async (id) => {
+  try {
+    const Idea = await ideaModel.find(id);
+    return Idea;
+  } catch (err) {
+    console.error("🚀 ~ file: idea.repository.js:18 ~ readIdea ~ err", err);
+    return err;
+  }
+};
+
+const updateIdea = async (id, data) => {
+  try {
+    const Idea = await ideaModel.findByIdAndUpdate(id, data);
+    return Idea;
+  } catch (err) {
+    console.error("🚀 ~ file: idea.repository.js:28 ~ updateIdea ~ err", err);
+    return err;
+  }
+};
+
+const deleteIdea = async (id) => {
+  try {
+    const Idea = await ideaModel.findByIdAndUpdate(id);
+    return Idea;
+  } catch (err) {
+    console.error("🚀 ~ file: idea.repository.js:38 ~ deleteIdea ~ err", err);
+    return err;
+  }
+};
+
+module.exports = {
+  createIdea,
+  readIdea,
+  updateIdea,
+  deleteIdea,
+};
