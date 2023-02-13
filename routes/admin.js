28ac336096834748e4d70f3a03821cee37b51ecf
hieupqgcd19971 <hieupqgcd19971@fpt.edu.vn>
2023-02-13 09:55:33 +0700

    fix: fix staff controller and create route admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
+const AccountController = require("../controllers/account.controller");
 
 const router = express.Router();
 
-router.get("/account", AccountController.displayListAccount);
-router.put("/account/edit", AccountController.displayListAccount);
-router.post("/account/create", AccountController.displayListAccount);
+// router.get("/account", AccountController.displayListAccount);
+// router.put("/account/edit", AccountController.displayListAccount);
+// router.post("/account/create", AccountController.displayListAccount);
 
-router.get("/terms", TermsController.displayListTerms);
-
-
+// router.get("/terms", TermsController.displayListTerms);
 
 module.exports = router;
