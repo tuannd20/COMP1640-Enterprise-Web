be87dd20f03d87c5aa77ed4549537ed38752cdb8
LePhuongTrung <trunglp2001@gmail.com>
2023-02-24 20:04:36 +0700

    fix: add create and update to staffIdeaService

--- controllers/interact.controller.js
+++ controllers/interact.controller.js
@@ -1,21 +1,15 @@
 const staffIdeaService = require("../services/staffIdea.service");
-const staffService = require("../services/staff.service");
-const categoryService = require("../services/category.service");
-const StaffIdeaModel = require("../database/models/StaffIdea");
-const sendMail = require("../utilities/sendMail");
-const Staff = require("../database/models/Staff");
 
 const LikeIdea = async (req, res) => {
   try {
     if (!req.body.IdIdea || !req.body.idStaff || !req.body.isLike) {
       return res.status(404).send("Missing required information");
     }
-    const check = await staffIdeaService.findOne({
+    await staffIdeaService.findOne({
       idStaff: req.body.idStaff,
       IdIdea: req.body.IdIdea,
     });
-    if (!check) {
-    }
+
     return res.status(200).send("Oke");
   } catch (err) {
     console.log("🚀 ~ file: idea.controller.js:256 ~ updateStatus ~ err:", err);
--- services/staffIdea.service.js
+++ services/staffIdea.service.js
@@ -1,17 +1,38 @@
 const StaffIdeaRepository = require("../repositories/staffIdea.repository");
-const { createTokenJwt } = require("../utilities/jwt");
 
-const findOne = async (data) => {
+const findOne = async (options) => {
   try {
-    const staff = await StaffIdeaRepository.findStaffIDea(data);
+    const staff = await StaffIdeaRepository.findStaffIDea(options);
 
     return staff;
   } catch (err) {
-    console.log("🚀 ~ file: staffIdea.service.js:10 ~ findOne ~ err:", err);
+    console.error("🚀 ~ file: staffIdea.service.js:10 ~ findOne ~ err:", err);
     return err;
   }
 };
 
+const createNew = async (data) => {
+  try {
+    const staff = await StaffIdeaRepository.createStaffIDea(data);
+
+    return staff;
+  } catch (err) {
+    console.error("🚀 ~ file: staffIdea.service.js:20 ~ createNew ~ err:", err);
+    return err;
+  }
+};
+const update = async (options, data) => {
+  try {
+    const staff = await StaffIdeaRepository.updateStaffIDea(options, data);
+
+    return staff;
+  } catch (err) {
+    console.error("🚀 ~ file: staffIdea.service.js:30 ~ update ~ err:", err);
+    return err;
+  }
+};
 module.exports = {
   findOne,
+  createNew,
+  update,
 };
